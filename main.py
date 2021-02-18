9cb2ae983cce7c8b5063553884e324c02ae52bd1
MARTIN_HRISTEV <mhristev03@gmail.com>
2021-02-18 22:29:02 +0200

    kanban init

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
