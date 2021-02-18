0ed751373d6bbc44bd99eb77eee2b174921f2d8c
Toma Milkov <toma.milkov@gmail.com>
2021-02-18 17:36:25 +0200

    File for probably working date

---
+++ maybe_working_date.py
@@ -0,0 +1,20 @@
+from datetime import date
+count = 0
+today = None
+
+def func():
+    global today
+    global count
+    if count == 0:
+        today = date.today()
+        print(today)
+        count += 1
+
+
+func()
+#print("Count: " + str(count))
+#print("Today: " + str(today))
+if today != date.today():
+    print("Today's date:", today)
+else:
+    print("In else")
